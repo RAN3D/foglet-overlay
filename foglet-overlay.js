e0a38f0c29dbae75de733655bc6641bffcf315a0
folkvir <dev.arnaudgrall@gmail.com>
2017-11-13 11:05:31 +0100

    clean code

--- foglet-overlay.js
+++ foglet-overlay.js
@@ -1,13 +1,5 @@
 'use strict';
 
-class A extends Map {
-	constructor () {
-		super();
-	}
-}
-
 module.exports = {
-	latencyVivaldiOverlay: require('./src/latency-vivaldi-overlay.js'),
-	foglet: require('foglet-core'),
-	MyMap: A
+	latencyVivaldiOverlay: require('./src/latency-vivaldi-overlay.js')
 }
--- tests/others/promise.js
+++
@@ -1,8 +0,0 @@
-
-let a  = new Promise((resolve, reject) => {
-  resolve ('toto', 'toto', 'titi');
-});
-
-a.then((a, b, c) => {
-  console.log(a, b, c);
-});
